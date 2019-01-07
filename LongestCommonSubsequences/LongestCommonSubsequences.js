d42574f601fd426dcac0bd4ecad4cc78399226b2
Thang Tran <trankimthang279@gmail.com>
2019-01-07 21:28:14 +0100

    Refactored according to ESLint.

--- LongestCommonSubsequences/LongestCommonSubsequences.js
+++ LongestCommonSubsequences/LongestCommonSubsequences.js
@@ -2,60 +2,69 @@
 module.exports.longestCommonSubSequences = longestCommonSubSequences;
 
 
-function longestCommonSubSequences(s1, s2) {
-    // Find sub-seq based on s1.
-    var subSeq1 = longestCommonSubSequencesHelper(s1, s2);
-    // Find sub-seq based on s2.
-    var subSeq2 = longestCommonSubSequencesHelper(s2, s1);
-
-    // Get longest sub seq.
-    return (subSeq1.length < subSeq2.length) ? subSeq2 : subSeq1;
-}
-
-
 /**
  * This function finds the sub sequence from 2 strings by
  * iterating each character from s1 and compare it with each
  * character from s2.
+ * @param {string} s1 : First string to be compared.
+ * @param {string} s2 : Second string to be compared.
+ * @return {string} : longest common sub sequence.
  */
-function longestCommonSubSequencesHelper(s1, s2) {
-    // If 1 of the string is empty, return an
-    // empty string.
-    if (s1.length == 0 || s2.length == 0)
-        return "";
+function longestCommonSubSequences(s1, s2) {
+  // Find sub-seq based on s1.
+  const subSeq1 = longestCommonSubSequencesImpl(s1, s2);
+  // Find sub-seq based on s2.
+  const subSeq2 = longestCommonSubSequencesImpl(s2, s1);
+
+  // Get longest sub seq.
+  return (subSeq1.length < subSeq2.length) ? subSeq2 : subSeq1;
+}
 
 
-    var result = [];
+/**
+ * This is the implementation of longestCommonSubSequences.
+ * @param {string} s1 : First string to be compared.
+ * @param {string} s2 : Second string to be compared.
+ * @return {string} : longest common sub sequence.
+ */
+function longestCommonSubSequencesImpl(s1, s2) {
+  // If 1 of the string is empty, return an
+  // empty string.
+  if (s1.length == 0 || s2.length == 0) {
+    return '';
+  }
 
-    for (var i = 0; i < s1.length; ++i) {
-        // Different start character gives different sub-sequence.
-        var newS1 = s1.substring(i);
-        var nextS2Index = 0;
-        var resultTemp = "";
 
-        for (var k = 0; k < newS1.length; ++k) {
-            for (var j = nextS2Index; j < s2.length; ++j) {
-                if (newS1[k] === s2[j]) {
-                    resultTemp += newS1[k];
-                    nextS2Index = j + 1;
+  const result = [];
 
-                    break;
-                }
-            }
+  for (let i = 0; i < s1.length; ++i) {
+    // Different start character gives different sub-sequence.
+    const newS1 = s1.substring(i);
+    let nextS2Index = 0;
+    let resultTemp = '';
 
-            // Return immediately if this is last char of
-            // s2 and there is a match.
-            if (nextS2Index === s2.length) 
-            {
-                break;
-            }
+    for (let k = 0; k < newS1.length; ++k) {
+      for (let j = nextS2Index; j < s2.length; ++j) {
+        if (newS1[k] === s2[j]) {
+          resultTemp += newS1[k];
+          nextS2Index = j + 1;
+
+          break;
         }
+      }
 
-        result.push (resultTemp);
+      // Return immediately if this is last char of
+      // s2 and there is a match.
+      if (nextS2Index === s2.length) {
+        break;
+      }
     }
 
-    var returnFunction = function(a,b){
-        return (a.length>b.length ? a:b);
-    }
-    return result.reduce( returnFunction );
+    result.push(resultTemp);
+  }
+
+  const returnFunction = function(a, b) {
+    return (a.length>b.length ? a:b);
+  };
+  return result.reduce( returnFunction );
 }
--- LongestCommonSubsequences/LongestCommonSubsequences.test.js
+++ LongestCommonSubsequences/LongestCommonSubsequences.test.js
@@ -1,46 +1,46 @@
 const {longestCommonSubSequences} = require('./LongestCommonSubsequences');
 
-test (`'ABAZDC', 'BACBAD'`, () => {
-    expect(longestCommonSubSequences ('ABAZDC', 'BACBAD'))
-    .toBe('ABAD');
+test(`'ABAZDC', 'BACBAD'`, () => {
+  expect(longestCommonSubSequences('ABAZDC', 'BACBAD'))
+      .toBe('ABAD');
 });
 
-test (`'aaaaaa', 'aa'`, () => {
-    expect(longestCommonSubSequences ('aaaaaa', 'aa'))
-    .toBe('aa');
+test(`'aaaaaa', 'aa'`, () => {
+  expect(longestCommonSubSequences('aaaaaa', 'aa'))
+      .toBe('aa');
 });
 
-test (`'AGGTAB', 'GXTXAYB'`, () => {
-    expect(longestCommonSubSequences ('AGGTAB', 'GXTXAYB'))
-    .toBe('GTAB');
+test(`'AGGTAB', 'GXTXAYB'`, () => {
+  expect(longestCommonSubSequences('AGGTAB', 'GXTXAYB'))
+      .toBe('GTAB');
 });
 
-test (`'BACBAD', 'ABAZDC'`, () => {
-    expect(longestCommonSubSequences ('BACBAD', 'ABAZDC'))
-    .toBe('ABAD');
+test(`'BACBAD', 'ABAZDC'`, () => {
+  expect(longestCommonSubSequences('BACBAD', 'ABAZDC'))
+      .toBe('ABAD');
 });
 
-test (`'aa', 'aaaaaa'`, () => {
-    expect(longestCommonSubSequences ('aa', 'aaaaaa'))
-    .toBe('aa');
+test(`'aa', 'aaaaaa'`, () => {
+  expect(longestCommonSubSequences('aa', 'aaaaaa'))
+      .toBe('aa');
 });
 
-test (`'GXTXAYB', 'AGGTAB'`, () => {
-    expect(longestCommonSubSequences ('GXTXAYB', 'AGGTAB'))
-    .toBe('GTAB');
+test(`'GXTXAYB', 'AGGTAB'`, () => {
+  expect(longestCommonSubSequences('GXTXAYB', 'AGGTAB'))
+      .toBe('GTAB');
 });
 
-test (`'AXJSHDKASRD', 'RXJSHDERASDJAB'`, () => {
-    expect(longestCommonSubSequences ('AXJSHDKASRD', 'RXJSHDERASDJAB'))
-    .toBe('XJSHDASD');
+test(`'AXJSHDKASRD', 'RXJSHDERASDJAB'`, () => {
+  expect(longestCommonSubSequences('AXJSHDKASRD', 'RXJSHDERASDJAB'))
+      .toBe('XJSHDASD');
 });
 
-test (`'RXJSHDERASDJAB', 'AXJSHDKASRD'`, () => {
-    expect(longestCommonSubSequences ('RXJSHDERASDJAB', 'AXJSHDKASRD'))
-    .toBe('XJSHDASD');
+test(`'RXJSHDERASDJAB', 'AXJSHDKASRD'`, () => {
+  expect(longestCommonSubSequences('RXJSHDERASDJAB', 'AXJSHDKASRD'))
+      .toBe('XJSHDASD');
 });
 
-test (`'JASHDERASDJAB', 'JASHDERASDJAB'`, () => {
-    expect(longestCommonSubSequences ('JASHDERASDJAB', 'JASHDERASDJAB'))
-    .toBe('JASHDERASDJAB');
+test(`'JASHDERASDJAB', 'JASHDERASDJAB'`, () => {
+  expect(longestCommonSubSequences('JASHDERASDJAB', 'JASHDERASDJAB'))
+      .toBe('JASHDERASDJAB');
 });

7548a7e4a89aa32c2fd5c261ee5001f35ef62e1e
Thang Tran <trankimthang279@gmail.com>
2019-01-08 22:46:47 +0100

    Refactored: change single quote to backtick.

--- LongestCommonSubsequences/LongestCommonSubsequences.test.js
+++ LongestCommonSubsequences/LongestCommonSubsequences.test.js
@@ -1,46 +1,46 @@
-const {longestCommonSubSequences} = require('./LongestCommonSubsequences');
+const {longestCommonSubSequences} = require(`./LongestCommonSubsequences`);
 
 test(`'ABAZDC', 'BACBAD'`, () => {
-  expect(longestCommonSubSequences('ABAZDC', 'BACBAD'))
-      .toBe('ABAD');
+  expect(longestCommonSubSequences(`ABAZDC`, `BACBAD`))
+      .toBe(`ABAD`);
 });
 
 test(`'aaaaaa', 'aa'`, () => {
-  expect(longestCommonSubSequences('aaaaaa', 'aa'))
-      .toBe('aa');
+  expect(longestCommonSubSequences(`aaaaaa`, `aa`))
+      .toBe(`aa`);
 });
 
 test(`'AGGTAB', 'GXTXAYB'`, () => {
-  expect(longestCommonSubSequences('AGGTAB', 'GXTXAYB'))
-      .toBe('GTAB');
+  expect(longestCommonSubSequences(`AGGTAB`, `GXTXAYB`))
+      .toBe(`GTAB`);
 });
 
 test(`'BACBAD', 'ABAZDC'`, () => {
-  expect(longestCommonSubSequences('BACBAD', 'ABAZDC'))
-      .toBe('ABAD');
+  expect(longestCommonSubSequences(`BACBAD`, `ABAZDC`))
+      .toBe(`ABAD`);
 });
 
 test(`'aa', 'aaaaaa'`, () => {
-  expect(longestCommonSubSequences('aa', 'aaaaaa'))
-      .toBe('aa');
+  expect(longestCommonSubSequences(`aa`, `aaaaaa`))
+      .toBe(`aa`);
 });
 
 test(`'GXTXAYB', 'AGGTAB'`, () => {
-  expect(longestCommonSubSequences('GXTXAYB', 'AGGTAB'))
-      .toBe('GTAB');
+  expect(longestCommonSubSequences(`GXTXAYB`, `AGGTAB`))
+      .toBe(`GTAB`);
 });
 
 test(`'AXJSHDKASRD', 'RXJSHDERASDJAB'`, () => {
-  expect(longestCommonSubSequences('AXJSHDKASRD', 'RXJSHDERASDJAB'))
-      .toBe('XJSHDASD');
+  expect(longestCommonSubSequences(`AXJSHDKASRD`, `RXJSHDERASDJAB`))
+      .toBe(`XJSHDASD`);
 });
 
 test(`'RXJSHDERASDJAB', 'AXJSHDKASRD'`, () => {
-  expect(longestCommonSubSequences('RXJSHDERASDJAB', 'AXJSHDKASRD'))
-      .toBe('XJSHDASD');
+  expect(longestCommonSubSequences(`RXJSHDERASDJAB`, `AXJSHDKASRD`))
+      .toBe(`XJSHDASD`);
 });
 
 test(`'JASHDERASDJAB', 'JASHDERASDJAB'`, () => {
-  expect(longestCommonSubSequences('JASHDERASDJAB', 'JASHDERASDJAB'))
-      .toBe('JASHDERASDJAB');
+  expect(longestCommonSubSequences(`JASHDERASDJAB`, `JASHDERASDJAB`))
+      .toBe(`JASHDERASDJAB`);
 });
